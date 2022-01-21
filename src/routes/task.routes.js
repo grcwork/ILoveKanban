56d29ab2cd3cdea4a83490de6cf3ef018550ab23
grcwork <grc.work123@gmail.com>
2022-01-20 23:18:00 -0300

    Now it is possible to add and delete tasks

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -7,4 +7,8 @@ router.patch('/move/', task.moveTask);
 
 router.patch('/edit/', task.editTask);
 
+router.post('/add/', task.addNewEmptyTask);
+
+router.delete('/delete/', task.deleteTask);
+
 module.exports = router;
